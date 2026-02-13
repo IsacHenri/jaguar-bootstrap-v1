14bd3a940fe68186d441159c485b7f3767d63568
IsacHenri <isacapdg@gmail.com>
2026-02-13 16:30:56 -0300

    update: index

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,13 +80,15 @@
       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
     }
   }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
+  if (scrollTop) {
+    scrollTop.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
     });
-  });
+  }
 
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
@@ -107,18 +109,26 @@
   /**
    * Init typed.js
    */
-  const selectTyped = document.querySelector('.typed');
-  if (selectTyped) {
-    let typed_strings = selectTyped.getAttribute('data-typed-items');
-    typed_strings = typed_strings.split(',');
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+  window.addEventListener('load', function () {
+    const selectTyped = document.querySelector('.typed');
+
+    if (selectTyped && typeof Typed !== 'undefined') {
+      const typed_strings = selectTyped
+        .getAttribute('data-typed-items')
+        .split(',')
+        .map(item => item.trim());
+
+      new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+      });
+    }
+  });
+
+
 
   /**
    * Animate the skills items on reveal
